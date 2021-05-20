c721fdd68f4e304f098ec38f1983cb420be3a153
themonty_회사 <ljs@hogun.co.kr>
2021-05-20 17:30:57 +0900

    Move Ball

---
+++ 01_Basic/First_Project/Assets/Components.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Components : MonoBehaviour
+{
+
+    Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>(); // Rigidbody 가져오기
+        //rigid.velocity = Vector3.right; // 속력 적용
+        //rigid.velocity = new Vector3(2, 4, 3);
+        //rigid.AddForce(Vector3.up * 50, ForceMode.Impulse); // AddForce(Vec) : Vec의 방향과 크기로 힘을 줌 FoorceMode : 힘을 주는 방식 (가속, 무게 반영)
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    // RigidBody 관련 코드는 FIxedUpdate에 작성
+    void FixedUpdate()
+    {
+        //rigid.velocity = new Vector3(2, 4, 3);
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    rigid.AddForce(Vector3.up * 50, ForceMode.Impulse);
+        //    Debug.Log(rigid.velocity);
+
+        //}
+
+        Vector3 vec = new Vector3(
+            Input.GetAxisRaw("Horizontal") * 5, 0, Input.GetAxisRaw("Vertical") * 5);
+
+        rigid.AddForce(vec, ForceMode.Impulse);
+
+        // 3. 회전력
+        // AddTorque(Vec) : Vec 방향을 축으로 회전력이 생김.
+        //rigid.AddTorque(Vector3.up);
+    }
+}
